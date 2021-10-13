abee2f80bd9e34eddf0ba2968fa48069641a67b7
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-10-13 16:31:04 -0400

    formatted

---
+++ script.js
@@ -0,0 +1,16 @@
+function sum(numOne, numTwo) {
+    const numOne = document.getElementById('firstNumber').value
+    const numTwo = document.getElementById('secondNumber').value
+    return numOne + numTwo
+
+}
+
+
+
+
+
+
+
+
+
+
