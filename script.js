a5e3d5583aded1bfb8fceb0cfd5b23ad4430c9c4
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-10-19 14:44:02 -0400

    added function list

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function sum() {
-
+    const operation = document.getElementById('function').value
     const total = Number(document.getElementById('firstNumber').value) + Number(document.getElementById('secondNumber').value)
-    alert('The answer is...' + total + '!')
+    //alert('The answer is...' + total + '!')
+    const functions = document.getElementById('total').value
+    functions.innerHTML = total
 
 }
+
+
 
 function punchLine() {
 
