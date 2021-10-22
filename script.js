1d0fd6855f3faca8f62dd0445ba9dbabe6a5726b
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-10-22 14:30:06 -0400

    not concise code, but does all the functions

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
 function sum() {
-    var num1 = Number(document.getElementsById('firstNumber').value);
-    var num2 = Number(document.getElementsById('secondNumber').value);
-    var operator = document.getElementsById('operation').value;
-    var total = num1 + operator + num2
-    if (operator == '+') {
 
-    }
-    else if (operator == '-') {
+    const total = Number(document.getElementById('firstNumber').value) + Number(document.getElementById('secondNumber').value)
+    alert('The answer is...' + total + '!')
+}
 
-    }
-    else if (operator == '/') {
+function sum() {
 
-    }
-    else if (operator == '*') {
+    const total = Number(document.getElementById('firstNumber').value) - Number(document.getElementById('secondNumber').value)
+    alert('The answer is...' + total + '!')
+}
 
-    }
-    else
-        aler('invalid')
+function sum() {
 
-    // var total = num1 + operators + num2;
-
-    //var num1 = Number(document.getElementsById('firstNumber').value);
-    //var num2 = Number(document.getElementsById('secondNumber').value);
-    //var total = Number(document.getElementsById('answer').value);
-    // var operators = document.getElementsById('operation').value;
-
-    // document.getElementsById('answer').value = (num1, operators, num2)
-    //var sum = Number(num1) + Number(num2);
+    const total = Number(document.getElementById('firstNumber').value) / Number(document.getElementById('secondNumber').value)
+    alert('The answer is...' + total + '!')
+}
 
 
+function sum() {
 
-    function punchLine() {
+    const total = Number(document.getElementById('firstNumber').value) * Number(document.getElementById('secondNumber').value)
+    alert('The answer is...' + total + '!')
 
-        const joker = 'With a COWculator'
-        alert(joker + '!')
-    }
+}
+function punchLine() {
+
+    const joker = 'With a COWculator'
+    alert(joker + '!')
+
+}
