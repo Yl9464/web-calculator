d89ce5308666797611b47a54007fa0db65856d4d
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-10-22 15:12:11 -0400

    working calculator

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 function sum() {
 
-    const total = Number(document.getElementById('firstNumber').value) + Number(document.getElementById('secondNumber').value)
-    alert('The answer is...' + total + '!')
+    const num1 = Number(document.getElementById('firstNumber').value)
+    //const functions = document.getElementById(operation)
+    const num2 = Number(document.getElementById('secondNumber').value)
+    const ops = document.getElementById('operation').value
+    // const operation = Number(document.getElementById('operation').value)
+
+    //alert(Number(num1) + Number(num2))
+    // alert(Number(num1) - Number(num2))
+    if (ops == 'addition') {
+        alert(num1 + num2)
+    } else if (ops == 'subtraction') {
+        alert(num1 - num2)
+    } else if (ops == 'divison') {
+        alert(num1 / num2)
+    } else if (ops == 'multiplication') {
+        alert(num1 * num2)
+    } else {
+        alert('unknown')
+    }
 }
 
-function sum() {
-
-    const total = Number(document.getElementById('firstNumber').value) - Number(document.getElementById('secondNumber').value)
-    alert('The answer is...' + total + '!')
-}
-
-function sum() {
-
-    const total = Number(document.getElementById('firstNumber').value) / Number(document.getElementById('secondNumber').value)
-    alert('The answer is...' + total + '!')
-}
-
-
-function sum() {
-
-    const total = Number(document.getElementById('firstNumber').value) * Number(document.getElementById('secondNumber').value)
-    alert('The answer is...' + total + '!')
-
-}
 function punchLine() {
 
     const joker = 'With a COWculator'
     alert(joker + '!')
-
 }
