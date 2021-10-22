45e11d1d4924f6ee3c44be417cf8b64d759633a9
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-10-22 11:27:48 -0400

    addition option working

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function sum() {
-    const total = Number(document.getElementById('firstNumber').value) + Number(document.getElementById('secondNumber').value)
-    alert('The answer is...' + total + '!')
+    var num1 = Number(document.getElementById('firstNumber').value);
+    var num2 = Number(document.getElementById('secondNumber').value);
 
+    var sum = Number(num1) + Number(num2);
+
+    alert(sum);
+    //const total = Number(document.getElementById('firstNumber').value) + Number(document.getElementById('secondNumber').value)
+    //alert('The answer is...' + total + '!')
 }
+
 
 function punchLine() {
 
