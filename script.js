12cc780c0e0fd0529bdf964bf72ee3c15b543825
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-10-22 11:37:28 -0400

    switch added

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ function sum() {
     var num1 = Number(document.getElementById('firstNumber').value);
     var num2 = Number(document.getElementById('secondNumber').value);
 
-    var sum = Number(num1) + Number(num2);
+    var sum = num1 + operation + num2
+    //var sum = Number(num1) + Number(num2);
+    switch (sum) {
+        case "add":
+            alert('the answer is' + sum);
+            break
 
-    alert(sum);
+        case "minus":
+            alert('the answer is' + sum);
+            break
+
+        default:
+            alert("unknown")
+    }
+    // alert(sum);
     //const total = Number(document.getElementById('firstNumber').value) + Number(document.getElementById('secondNumber').value)
     //alert('The answer is...' + total + '!')
 }
