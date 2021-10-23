7807c516b245ba5c5a1b7d616a1da0b026707784
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-10-22 22:56:47 -0400

    it works

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-function sum() {
+function calculate() {
 
-    const num1 = Number(document.getElementById('firstNumber').value)
-    //const functions = document.getElementById(operation)
-    const num2 = Number(document.getElementById('secondNumber').value)
-    const ops = document.getElementById('operation').value
-    // const operation = Number(document.getElementById('operation').value)
+    var num1 = parseFloat(document.getElementById('firstNumber').value)
+    var num2 = parseFloat(document.getElementById('secondNumber').value)
+    var ops = document.getElementById('operation').value
 
-    //alert(Number(num1) + Number(num2))
-    // alert(Number(num1) - Number(num2))
-    if (ops == 'addition') {
-        alert(num1 + num2)
+
+    if (ops === 'addition') {
+        document.getElementById('result').value = num1 + num2;
     } else if (ops == 'subtraction') {
-        alert(num1 - num2)
+        document.getElementById('result').value = num1 - num2;
     } else if (ops == 'divison') {
-        alert(num1 / num2)
+        document.getElementById('result').value = num1 / num2;
     } else if (ops == 'multiplication') {
-        alert(num1 * num2)
+        document.getElementById('result').value = num1 * num2;
     } else {
         alert('unknown')
     }
